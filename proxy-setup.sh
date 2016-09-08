47b6a8826841653eeaabffa4b991ccd93f02a65e
Bob van Luijt <bob@kubrickolo.gy>
2016-09-08 11:53:47 +0200

    Update proxy-setup.sh

--- proxy-setup.sh
+++ proxy-setup.sh
@@ -16,6 +16,9 @@ apt-get update -qq -y
 # Install MYSQL client
 apt-get install mysql-client-core-5.7 -qq -y
 
+# Install jq for parsing jquery
+apt-get install jq -qq- y
+
 # Install Docker deps
 apt-get install apt-transport-https ca-certificates -qq -y
 
