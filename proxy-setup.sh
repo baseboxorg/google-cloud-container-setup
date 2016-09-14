cb0c7cd1b477cb905f4a5ce99d6a26d09ec6501a
Bob van Luijt <bob@kubrickolo.gy>
2016-09-14 21:22:17 +0200

    Update proxy-setup.sh

--- proxy-setup.sh
+++ proxy-setup.sh
@@ -22,6 +22,9 @@ curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 # Update
 apt-get update -qq -y
 
+# Install security updates
+apt-get unattended-upgrades -d -qq -y
+
 # Install MYSQL client and set pass and host
 apt-get install mysql-client-5.7 -qq -y
 echo "What is the mysql root password: "
