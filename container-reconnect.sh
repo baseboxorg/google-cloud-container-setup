262823358d245a8069a88c592af7da3ceeda63ea
Bob van Luijt <bob@kubrickolo.gy>
2016-10-06 15:31:46 +0200

    gh-14 create bash for reconnecting

---
+++ container-reconnect.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+###############
+#
+# Bash script for reconnecting new Docker containers and NGINX proxys
+# Author: Bob van Luijt
+# Readme: https://github.com/bobvanluijt/Docker-multi-wordpress-hhvm-google-cloud
+#
+###############
