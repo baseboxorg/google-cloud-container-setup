455d74ee272c4d765238ce65aee353493f8f9133
Bob van Luijt <bob@kubrickolo.gy>
2016-10-13 14:35:43 +0200

    gh-25 Set Docker Compose files

--- proxy-setup.sh
+++ proxy-setup.sh
@@ -73,6 +73,10 @@ apt-get install docker-engine -qq -y
 # Start the Docker service
 service docker start
 
+# Install Docker compose
+curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
 # Install NGINX
 apt-get install nginx -qq -y
 
