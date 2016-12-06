6efe65729d5a2da84f5f679e989207cabf6717c5
Bob van Luijt <bob@kubrickolo.gy>
2016-12-06 12:03:23 +0100

    DIO-55: Add ssh creation

--- dorelcloudtool.sh
+++ dorelcloudtool.sh
@@ -594,7 +594,7 @@ if [[ "$TASK" == "create_worker" ]]
 then
 
     # Set the sub-project id
-	SetSubProjectId
+    SetSubProjectId
 
     # Set worker ID
     GenerateUid
@@ -620,6 +620,9 @@ then
     SQLIP=$(gcloud sql --format json instances describe "${SQLID}" | jq -r '.ipAddresses[0].ipAddress')
     GenerateSqlPassword
     echo $(((100/5)*4)) | dialog --title "$TITLE" --backtitle "$BACKTITLE" --gauge "Setting up Docker, Nginx and Let's Encrypt on the worker (might take some time)" 10 70 0
+    
+    # This script will create the ssh key files needed to login
+    gcloud compute ssh dorel-io--limping-verdant--docker-worker-86ee01-n8db --command "ls"
     
     # Exec the setup
     ssh -tt -i ~/.ssh/google_compute_engine -oConnectTimeout=600 -oStrictHostKeyChecking=no ubuntu@${WORKERID} <<EOF
