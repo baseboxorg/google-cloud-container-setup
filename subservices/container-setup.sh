d8e33648b9761683d98bbe676635cf227351e03f
Bob van Luijt <bob@kubrickolo.gy>
2016-12-19 18:14:54 +0100

    DIO-283: Add settings for load balancer in wp config

--- subservices/container-setup.sh
+++ subservices/container-setup.sh
@@ -163,6 +163,9 @@ docker build -f ~/container-setup/subservices/Dorel-Dockerfiles/wordpress-nginx/
 containerWp=$(docker run -v ${WPACCESSURL}:/var/www/WordPress -d wordpress-gcloud) >> /var/log/wordpress-gcloud/${ACCESSURL}.log
 docker exec ${containerWp} /bin/sh -c 'mv -v /var/www/WordPressPre/* /var/www/WordPress/' >> /var/log/wordpress-gcloud/${ACCESSURL}.log
 
+## Set the load balancer settings
+mysql -e "INSERT INTO ${DBNAME}.wp_options (option_value, option_name) VALUES ('a:3:{s:9:\"fix_level\";s:6:\"simple\";s:9:\"proxy_fix\";s:22:\"HTTP_X_FORWARDED_PROTO\";s:12:\"fix_specific\";a:1:{s:9:\"woo_https\";i:1;}}', 'ssl_insecure_content_fixer');"
+
 ## Get the IP of the newly created container
 ipWp=$(docker inspect "$containerWp" | jq -r '.[0].NetworkSettings.IPAddress') >> /var/log/wordpress-gcloud/${ACCESSURL}.log
 
